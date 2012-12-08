09314e3dad286c3584a29da72c2dbe2e4788647b
OutOfBrain <outofbrain@gmail.com>
2012-12-08 14:42:02 +0100

    solving the first problem: dna

--- rosalinedolve.dart
+++ rosalinedolve.dart
@@ -1,5 +1,34 @@
 import 'dart:html';
 
+
+class RosalindSolver {
+  dna(String input) {
+    // init counter
+    var counter = new Map();
+    counter['A'] = 0;
+    counter['C'] = 0;
+    counter['G'] = 0;
+    counter['T'] = 0;
+
+    for (var char in input.splitChars()) {
+      if (counter.containsKey(char)) { // necessary since dart does not linke if element does not exist yet
+        counter[char]+=1;
+      }
+    }
+
+    // since Map.values is not stable or anything
+    return '${counter["A"]} ${counter["C"]} ${counter["G"]} ${counter["T"]}';
+  }
+}
+
 void main() {
-  
+  // create solver
+  RosalindSolver solver = new RosalindSolver();
+
+  // get elements
+  TextAreaElement input  = query('#input');
+  TextAreaElement output = query('#output');
+
+  // register events
+  input.on.input.add((event) { output.value = solver.dna(input.value); });
 }
