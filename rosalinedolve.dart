0623e05bc0ea0de7a0e0902555956f89635b219d
OutOfBrain <outofbrain@gmail.com>
2012-12-10 18:55:36 +0100

    added problem switcher

--- rosalinedolve.dart
+++ rosalinedolve.dart
@@ -7,15 +7,20 @@ class RosalindSolver {
     var counter = {'A' : 0, 'C' : 0, 'G' : 0, 'T' : 0};
 
     for (var char in input.splitChars()) {
-      if (counter.containsKey(char)) { // necessary since dart does not linke if element does not exist yet
-        counter[char]+=1;
+      if (counter.containsKey(char)) { // necessary since dart does not linke when element does not exist yet
+        ++counter[char];
       }
     }
 
     // since Map.values is not stable or anything
     return '${counter["A"]} ${counter["C"]} ${counter["G"]} ${counter["T"]}';
   }
+
+  rna(String input) {
+    return '';
+  }
 }
+
 
 void main() {
   // create solver
@@ -25,6 +30,20 @@ void main() {
   TextAreaElement input  = query('#input');
   TextAreaElement output = query('#output');
 
+  SelectElement problem  = query('#problem');
+
   // register events
-  input.on.input.add((event) { output.value = solver.dna(input.value); });
+  input.on.input.add((event) {
+    var solveMethod = null;
+
+    // get the current problem
+    switch (problem.value) {
+      case 'dna': solveMethod = solver.dna; break;
+      case 'rna': solveMethod = solver.rna; break;
+    }
+
+    if (solveMethod != null) {
+      output.value = solveMethod(input.value);
+    }
+  });
 }
